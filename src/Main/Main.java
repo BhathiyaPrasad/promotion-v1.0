585c7b79acbfb5de0a4afceb7ff70c3c038a1655
Bhathiya Prasad <90783913+BhathiyaPrasad@users.noreply.github.com>
2024-05-22 11:21:56 +0530

    create the buisness logic of the system to retrieve the increasment of the whole sale discount Parallel to the buying quantity of the User

--- src/Main/Main.java
+++ src/Main/Main.java
@@ -1,6 +1,11 @@
+package Main;
+
+import connection.database;
+import promotion.InvoiceItems;
 
 public class Main {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
+
     }
 }
