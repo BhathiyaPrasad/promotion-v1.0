c485f3db703dcda129fdd7e010a3e8d4e52fabc2
Bhathiya Prasad <90783913+BhathiyaPrasad@users.noreply.github.com>
2024-05-22 09:17:45 +0530

    creating database connection

--- promotion-java-1.0/src/database.java
+++ promotion-java-1.0/src/database.java
@@ -1,22 +1,68 @@
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 
 public class database {
-    static final String DB_URL = "jdbc:mysql://localhost:3307/fsms-data";
+    // Database URL
+    static final String DB_URL = "jdbc:mysql://localhost:3307/data";
+    // Database credentials
     static final String USER = "PosUser";
     static final String PASS = "MasterPos1212";
 
     public static void main(String[] args) {
-        try (Connection conn = DriverManager.getConnection(DB_URL, USER, PASS);
-                Statement stmt = conn.createStatement();) {
-            String sql = "Select * from " + DB_URL;
-            stmt.executeUpdate(sql);
-            System.out.println("Database created Successfully");
-            System.out.print(sql);
-        } catch (SQLException e) {
+        Connection conn = null;
+        Statement stmt = null;
+        try {
+            // Register JDBC driver
+            Class.forName("com.mysql.cj.jdbc.Driver");
+
+            // Open a connection
+            System.out.println("Connecting to the database...");
+            conn = DriverManager.getConnection(DB_URL, USER, PASS);
+
+            // Execute a query
+            System.out.println("Creating statement...");
+            stmt = conn.createStatement();
+            String sql = "SELECT item_id, item_name FROM items";
+            ResultSet rs = stmt.executeQuery(sql);
+
+            // Extract data from result set
+            while (rs.next()) {
+                // Retrieve by column name
+                int id = rs.getInt("item_id");
+                String name = rs.getString("item_name");
+
+                // Display values
+                System.out.print("ID: " + id);
+                System.out.print(", Name: " + name);
+
+            }
+            // Clean-up environment
+            rs.close();
+            stmt.close();
+            conn.close();
+        } catch (SQLException se) {
+            // Handle errors for JDBC
+            se.printStackTrace();
+        } catch (Exception e) {
+            // Handle errors for Class.forName
             e.printStackTrace();
-        }
+        } finally {
+            // Finally block used to close resources
+            try {
+                if (stmt != null)
+                    stmt.close();
+            } catch (SQLException se2) {
+            } // Nothing we can do
+            try {
+                if (conn != null)
+                    conn.close();
+            } catch (SQLException se) {
+                se.printStackTrace();
+            } // End finally try
+        } // End try
+        System.out.println("Goodbye!");
     }
 }
