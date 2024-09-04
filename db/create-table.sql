1dd49399f81097efb18f5f037fcf35e1b865a5de
Bhathiya Prasad <90783913+BhathiyaPrasad@users.noreply.github.com>
2024-09-04 17:22:35 +0530

    format

--- db/create-table.sql
+++ db/create-table.sql
@@ -1,7 +1,6 @@
 -- Drop existing tables if they exist
 DROP TABLE IF EXISTS `promotion`;
 DROP TABLE IF EXISTS `promotion_items`;
-
 -- Create promotion table
 CREATE TABLE `promotion` (
     `Pr-id` INT NOT NULL AUTO_INCREMENT,
@@ -11,7 +10,6 @@ CREATE TABLE `promotion` (
     `Created_Time` TEXT,
     PRIMARY KEY (`Pr-id`)
 );
-
 -- Create promotion_items table
 CREATE TABLE `promotion_items` (
     `Pr-id` INT NOT NULL AUTO_INCREMENT,
